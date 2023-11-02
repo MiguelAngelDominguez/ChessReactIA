8682da671efd66c9d271b71c9935a945b7be3677
pana-miguel <miguelangel.dominguez@unheval.pe>
2023-11-01 23:36:37 -0500

    Se creo un nuevo componente que crea tableros de ajedrez con las piezas en una pisicion especifica

--- src/ChessboardBody.jsx
+++ src/ChessboardBody.jsx
@@ -2,74 +2,32 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { Chess } from "chess.js"
 import Chessboard from "chessboardjsx";
-import { roughSquare } from "./customRough";
+
 
 const chess = new Chess()
 
-class RandomVsRandom extends Component {
-  static propTypes = { children: PropTypes.func };
-
-  state = { fen: "start" };
-
-  componentDidMount() {
-    this.game = new Chess();
-    setTimeout(() => this.makeRandomMove(), 1000);
-  }
-
-  componentWillUnmount() {
-    window.clearTimeout(this.timer());
-  }
-
-  timer = () => window.setTimeout(this.makeRandomMove, 1000);
-
-  makeRandomMove = () => {
-    let possibleMoves = this.game.moves();
-
-    // exit if the game is over
-    if (
-      this.game.isGameOver() === true ||
-      this.game.isDraw() === true ||
-      possibleMoves.length === 0
-    )
-      return;
-
-    let randomIndex = Math.floor(Math.random() * possibleMoves.length);
-    this.game.move(possibleMoves[randomIndex]);
-    this.setState({ fen: this.game.fen() });
-
-    this.timer();
-  };
-
-  render() {
-    const { fen } = this.state;
-    return this.props.children({ position: fen });
-  }
-}
-
-export const ChessboardBody = () => {
+export const ChessboardBody = ({w,position,roughSquare,id}) => {
 
   return (
-    <div>
-      <RandomVsRandom>
-        {({ position }) => (
-          <Chessboard
-            // Esta linea de codigo coloca piezas de muestra 
-            // fuera del tablero
-            // sparePieces={true}
-            width={500}
-            id="random"
-            position={position}
-            transitionDuration={300}
-            roughSquare={roughSquare}
-            boardStyle={{
-              borderRadius: "5px",
-              boxShadow: `0 5px 15px rgba(0, 0, 0, 0.5)`
-            }}
-            lightSquareStyle={{ backgroundColor: "rgba(25, 202, 233, 0.671)" }}
-            darkSquareStyle={{ backgroundColor: "rgb(20, 75, 92)" }}
-          />
-        )}
-      </RandomVsRandom>
+    <div id={id} className="board">
+      <Chessboard
+        // Esta linea de codigo coloca piezas de muestra 
+        // fuera del tablero
+        // sparePieces={true}
+        width={w}
+        id="random"
+        
+        position={position}
+        transitionDuration={300}
+        roughSquare={roughSquare}
+        boardStyle={{
+          borderRadius: "5px",
+          boxShadow: `0 5px 15px rgba(0, 0, 0, 0.5)`
+        }}
+        lightSquareStyle={{ backgroundColor: "rgba(25, 202, 233, 0.671)" }}
+        darkSquareStyle={{ backgroundColor: "rgb(20, 75, 92)" }}
+      />
     </div>
+    
   )
 }
