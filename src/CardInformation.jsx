d5eb5be38d2fbc1ce67db4ef12674d681355d6d3
pana-miguel <miguelangel.dominguez@unheval.pe>
2023-11-22 22:47:55 -0500

    Se agrego el parametro icon

--- src/CardInformation.jsx
+++ src/CardInformation.jsx
@@ -1,16 +1,14 @@
 import React from "react";
 import "./CardInformation.css";
 
-export const CardInformation = ({title,text}) => {
+export const CardInformation = ({ title, text ,icon}) => {
   return (
     <div>
       <article className="CardInformation flex max-w-xl flex-col items-start justify-between">
         <div className="relative flex items-center gap-x-4">
-          <img
-            src="https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-            alt=""
-            className="h-10 w-10 rounded-full bg-gray-50"
-          />
+          <div className="h-10 w-10 rounded-full bg-gray-50">
+            <i className={icon}></i>
+          </div>
         </div>
         <div className="group relative">
           <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
