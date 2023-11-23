5f662d0eefef7ed4bfc038949a25237414705430
pana-miguel <miguelangel.dominguez@unheval.pe>
2023-11-22 22:57:38 -0500

    Se agregaron las dos paginas visibles para el enrutador

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Header } from '../Header.jsx'
+import { Body } from '../Body.jsx'
+import { Footer } from '../Footer.jsx'
+
+export const Home = () => {
+    return (
+        <div>
+            <Header />
+            <Body />
+            {/* <Footer /> */}
+        </div>
+    )
+}
---
+++ src/pages/PlayChess.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { ChessboardPlay } from '../ChessboardPlay.jsx'
+import { Header } from '../Header.jsx'
+
+export const PlayChess = () => {
+    return (
+        <div>
+            <Header />
+            <ChessboardPlay />
+        </div>
+    )
+}
