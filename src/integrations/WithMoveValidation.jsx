6786743e4b2cba9757c538d66518ea61340d6527
pana-miguel <miguelangel.dominguez@unheval.pe>
2023-11-22 22:54:04 -0500

    Minimax 2.0 mejora notable en el algoritmo

--- src/integrations/WithMoveValidation.jsx
+++ src/integrations/WithMoveValidation.jsx
@@ -4,6 +4,79 @@ import { Chess } from "chess.js"; // Importar Chess desde "chess.js"
 
 import Chessboard from "chessboardjsx";  // Importar Chessboard desde "chessboardjsx"
 
+const pieceSquareTable = {
+    p: [
+        0, 0, 0, 0, 0, 0, 0, 0,
+        50, 50, 50, 50, 50, 50, 50, 50,
+        10, 10, 20, 30, 30, 20, 10, 10,
+        5, 5, 10, 25, 25, 10, 5, 5,
+        0, 0, 0, 20, 20, 0, 0, 0,
+        5, -5, -10, 0, 0, -10, -5, 5,
+        5, 10, 10, -20, -20, 10, 10, 5,
+        0, 0, 0, 0, 0, 0, 0, 0
+    ],
+    n: [
+        -50, -40, -30, -30, -30, -30, -40, -50,
+        -40, -20, 0, 0, 0, 0, -20, -40,
+        -30, 0, 10, 15, 15, 10, 0, -30,
+        -30, 5, 15, 20, 20, 15, 5, -30,
+        -30, 0, 15, 20, 20, 15, 0, -30,
+        -30, 5, 10, 15, 15, 10, 5, -30,
+        -40, -20, 0, 5, 5, 0, -20, -40,
+        -50, -40, -30, -30, -30, -30, -40, -50
+    ],
+    b: [
+        -20, -10, -10, -10, -10, -10, -10, -20,
+        -10, 0, 0, 0, 0, 0, 0, -10,
+        -10, 0, 5, 10, 10, 5, 0, -10,
+        -10, 5, 5, 10, 10, 5, 5, -10,
+        -10, 0, 10, 10, 10, 10, 0, -10,
+        -10, 10, 10, 10, 10, 10, 10, -10,
+        -10, 5, 0, 0, 0, 0, 5, -10,
+        -20, -10, -10, -10, -10, -10, -10, -20
+    ],
+    r: [
+        0, 0, 0, 0, 0, 0, 0, 0,
+        5, 10, 10, 10, 10, 10, 10, 5,
+        -5, 0, 0, 0, 0, 0, 0, -5,
+        -5, 0, 0, 0, 0, 0, 0, -5,
+        -5, 0, 0, 0, 0, 0, 0, -5,
+        -5, 0, 0, 0, 0, 0, 0, -5,
+        -5, 0, 0, 0, 0, 0, 0, -5,
+        0, 0, 0, 5, 5, 0, 0, 0
+    ],
+    q: [
+        -20, -10, -10, -5, -5, -10, -10, -20,
+        -10, 0, 0, 0, 0, 0, 0, -10,
+        -10, 0, 5, 5, 5, 5, 0, -10,
+        -5, 0, 5, 5, 5, 5, 0, -5,
+        0, 0, 5, 5, 5, 5, 0, -5,
+        -10, 5, 5, 5, 5, 5, 0, -10,
+        -10, 0, 5, 0, 0, 0, 0, -10,
+        -20, -10, -10, -5, -5, -10, -10, -20
+    ],
+    k_midgame: [
+        -30, -40, -40, -50, -50, -40, -40, -30,
+        -30, -40, -40, -50, -50, -40, -40, -30,
+        -30, -40, -40, -50, -50, -40, -40, -30,
+        -30, -40, -40, -50, -50, -40, -40, -30,
+        -20, -30, -30, -40, -40, -30, -30, -20,
+        -10, -20, -20, -20, -20, -20, -20, -10,
+        20, 20, 0, 0, 0, 0, 20, 20,
+        20, 30, 10, 0, 0, 10, 30, 20
+    ],
+    k_endgame: [
+        -50, -40, -30, -20, -20, -30, -40, -50,
+        -30, -20, -10, 0, 0, -10, -20, -30,
+        -30, -10, 20, 30, 30, 20, -10, -30,
+        -30, -10, 30, 40, 40, 30, -10, -30,
+        -30, -10, 30, 40, 40, 30, -10, -30,
+        -30, -10, 20, 30, 30, 20, -10, -30,
+        -30, -30, 0, 0, 0, 0, -30, -30,
+        -50, -30, -30, -30, -30, -30, -30, -50
+    ],
+};
+
 class HumanVsHuman extends Component {  // Crear la clase HumanVsHuman que extiende de Component
     state = {
         fen: "start",  // Estado inicial del tablero de ajedrez
@@ -63,9 +136,10 @@ class HumanVsHuman extends Component {  // Crear la clase HumanVsHuman que extie
         let move = this.game.move({
             from: sourceSquare,
             to: targetSquare,
-            promotion: "q"  // Siempre promover a una reina por simplicidad
+            promotion: "k"  // Siempre promover a una reina por simplicidad
         });
         console.log(this.game.history().length)
+        console.log(this.game.turn())
         if (this.game.turn() === "w" && this.game.history().length === 0) {
             const moves = this.game.moves({ verbose: true });
             const randomIndex = Math.floor(Math.random() * moves.length);
@@ -88,7 +162,10 @@ class HumanVsHuman extends Component {  // Crear la clase HumanVsHuman que extie
         }));
         // Implementa la función minimax para el turno de las blancas
         if (this.game.turn() === "w") {
-            const { move: bestMove } = this.minimax(this.game, 3, -Infinity, Infinity, false);
+            const { move: bestMove, value: bestValue } = this.minimax(this.game, 4, -Infinity, Infinity, true);
+            console.log("bestMove: ", bestMove)
+            console.log("bestValue: ", bestValue)
+
             this.game.move(bestMove);
             this.setState(({ history, pieceSquare }) => ({
                 fen: this.game.fen(),
@@ -98,7 +175,7 @@ class HumanVsHuman extends Component {  // Crear la clase HumanVsHuman que extie
         }
     };
 
-    minimax = (game, depth, alpha, beta, maximizingPlayer) => {
+    minimax = (game, depth, alpha, beta, maximizingPlayer, lastPieceSquare) => {
         if (depth === 0 || game.game_over()) {
             return { move: null, value: this.evaluateBoard(game) };
         }
@@ -106,31 +183,55 @@ class HumanVsHuman extends Component {  // Crear la clase HumanVsHuman que extie
         let bestMove = null;
         let bestValue = maximizingPlayer ? -Infinity : Infinity;
 
-        const moves = game.moves();
+        const playerColor = maximizingPlayer ? "w" : "b";
+        const moves = [];
+
+        // Generar todos los movimientos posibles para las piezas del jugador actual
+        game.SQUARES.forEach((square) => {
+            const piece = game.get(square);
+
+            if (piece && piece.color === playerColor) {
+                const pieceMoves = game.moves({ square, verbose: true });
+
+                pieceMoves.forEach((move) => {
+                    moves.push(move);
+                });
+            }
+        });
+
         for (let i = 0; i < moves.length; i++) {
             const move = moves[i];
+
             game.move(move);
             const value = this.minimax(
                 game,
                 depth - 1,
                 alpha,
                 beta,
-                !maximizingPlayer
+                !maximizingPlayer,
+                move.to
             ).value;
             game.undo();
 
+            // Introduce una probabilidad ponderada para elegir entre diferentes piezas
+            const probability = Math.random();
+            const useDifferentPiece = probability > 0.7;
+
+            if (useDifferentPiece || i === 0) {
+                bestMove = move;
+                bestValue = value;
+            } else if (maximizingPlayer && value > bestValue) {
+                bestMove = move;
+                bestValue = value;
+            } else if (!maximizingPlayer && value < bestValue) {
+                bestMove = move;
+                bestValue = value;
+            }
+
             if (maximizingPlayer) {
-                if (value > bestValue) {
-                    bestMove = move;
-                    bestValue = value;
-                }
-                alpha = Math.max(alpha, value);
+                alpha = Math.max(alpha, bestValue);
             } else {
-                if (value < bestValue) {
-                    bestMove = move;
-                    bestValue = value;
-                }
-                beta = Math.min(beta, value);
+                beta = Math.min(beta, bestValue);
             }
 
             if (beta <= alpha) {
@@ -141,10 +242,114 @@ class HumanVsHuman extends Component {  // Crear la clase HumanVsHuman que extie
         return { move: bestMove, value: bestValue };
     };
 
+    // minimax = (game, depth, alpha, beta, maximizingPlayer) => {
+    //     if (depth === 0 || game.game_over()) {
+    //         return { move: null, value: this.evaluateBoard(game) };
+    //     }
+
+    //     let bestMove = null;
+    //     let bestValue = maximizingPlayer ? -Infinity : Infinity;
+
+    //     const playerColor = maximizingPlayer ? "w" : "b";
+    //     const moves = game.moves({ verbose: true });
+
+    //     // Shuffle the array of moves for randomness
+    //     for (let i = moves.length - 1; i > 0; i--) {
+    //         const j = Math.floor(Math.random() * (i + 1));
+    //         [moves[i], moves[j]] = [moves[j], moves[i]];
+    //     }
+
+    //     for (let i = 0; i < moves.length; i++) {
+    //         const move = moves[i];
+
+    //         if (game.get(move.from).color === playerColor) {
+    //             game.move(move);
+    //             const value = this.minimax(
+    //                 game,
+    //                 depth - 1,
+    //                 alpha,
+    //                 beta,
+    //                 !maximizingPlayer
+    //             ).value;
+    //             game.undo();
+
+    //             if (maximizingPlayer) {
+    //                 if (value > bestValue) {
+    //                     bestMove = move;
+    //                     bestValue = value;
+    //                 }
+    //                 alpha = Math.max(alpha, bestValue);
+    //             } else {
+    //                 if (value < bestValue) {
+    //                     bestMove = move;
+    //                     bestValue = value;
+    //                 }
+    //                 beta = Math.min(beta, bestValue);
+    //             }
+
+    //             if (beta <= alpha) {
+    //                 break;
+    //             }
+    //         }
+    //     }
+
+    //     return { move: bestMove, value: bestValue };
+    // };
+
+    isSquareProtected = (game, square, protectorColor) => {
+        const opponentsColor = protectorColor === "w" ? "b" : "w";
+
+        // Obtener todas las piezas del oponente
+        const opponentsPieces = game.SQUARES.map((s) => game.get(s)).filter(
+            (piece) => piece && piece.color === opponentsColor
+        );
+
+        // Verificar si alguna de las piezas del oponente protege la casilla
+        for (const opponentPiece of opponentsPieces) {
+            const opponentMoves = game.moves({
+                square: opponentPiece.square,
+                verbose: true,
+            });
+
+            for (const move of opponentMoves) {
+                if (move.to === square) {
+                    return true; // La casilla está protegida por una pieza más grande
+                }
+            }
+        }
+
+        return false;
+    };
+
+
+    canCapturePiece = (game, square) => {
+        const piece = game.get(square);
+        const opponentColor = piece.color === "w" ? "b" : "w";
+
+        // Obtener movimientos posibles para la pieza
+        const moves = game.moves({ square, verbose: true });
+
+        // Evaluar cada movimiento para ver si captura una pieza del oponente
+        for (const move of moves) {
+            const targetSquare = move.to;
+            const targetPiece = game.get(targetSquare);
+
+            if (targetPiece && targetPiece.color === opponentColor) {
+                // Comprobar si la pieza objetivo está en una posición vulnerable
+                if (!this.isSquareProtected(game, targetSquare, opponentColor)) {
+                    return true; // Puede capturar una pieza del oponente
+                }
+            }
+        }
+
+        return false;
+    };
+
     evaluateBoard = (game) => {
         // Count the material
         let material = 0;
         const board = game.board ? game.board() : [];
+
         for (let i = 0; i < board.length; i++) {
             for (let j = 0; j < board[i].length; j++) {
                 const piece = board[i][j];
@@ -154,8 +359,33 @@ class HumanVsHuman extends Component {  // Crear la clase HumanVsHuman que extie
                 }
             }
         }
+
+        // Add positional evaluation based on piece-square tables
+        for (let i = 0; i < board.length; i++) {
+            for (let j = 0; j < board[i].length; j++) {
+                const piece = board[i][j];
+                if (piece !== null) {
+                    const value =
+                        piece.color === "w"
+                            ? piece.value + pieceSquareTable[piece.type][i * 8 + j]
+                            : -piece.value - pieceSquareTable[piece.type][i * 8 + j];
+                    material += value;
+                }
+            }
+        }
+
+        // Evaluar oportunidades de captura
+        const canCapture = game.SQUARES.some((square) => {
+            const piece = game.get(square);
+            return piece && piece.color === "w" && this.canCapturePiece(game, square);
+        });
+
+        // Ponderar la evaluación basada en las oportunidades de captura
+        material += canCapture ? 10 : 0;
+
         return material;
     };
+
 
     onMouseOverSquare = (square) => {
         // Obtener la lista de movimientos posibles para este cuadro
@@ -249,7 +479,7 @@ export const WithMoveValidation = () => {  // Exportar la función WithMoveValid
                 }) => (  // Función de renderizado
                     <Chessboard  // Componente Chessboard
                         id="humanVsHuman"  // ID "humanVsHuman"
-                        width={320}  // Ancho 320
+                        width={600}  // Ancho 320
                         position={position}  // Posición
                         onDrop={onDrop}  // Función onDrop
                         onMouseOverSquare={onMouseOverSquare}  // Función onMouseOverSquare
